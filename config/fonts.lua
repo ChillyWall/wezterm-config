76fb561b515310b2f299ff61b5c59d923d32fffd
Chilly <chillywall@163.com>
2025-02-19 16:19:06 +0800

    add Chinese fallback font

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,15 +2,17 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
-local font_family = 'FiraCode Nerd Font'
-local font_size = platform.is_mac and 12 or 10
 
 return {
-  font = wezterm.font({
-    family = font_family,
-    weight = 'Medium',
+  font = wezterm.font_with_fallback({
+    {
+      family = 'FiraCode Nerd Font',
+      weight = 'Medium',
+    },
+    'HYZiKuTangSongKeBenLiKai',
   }),
-  font_size = font_size,
+
+  font_size = platform.is_mac and 12 or 10,
 
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
   freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
